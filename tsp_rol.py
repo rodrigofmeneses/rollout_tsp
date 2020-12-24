41b7f4ac493b7adf39a54a139e203c3d4b56a356
Rodrigo Meneses <rodrigo.menesesufc@gmail.com>
2020-12-24 17:54:59 -0300

    add experiments

--- tsp_rol.py
+++ tsp_rol.py
@@ -5,7 +5,6 @@ import networkx as nx
 import random as rd
 import os
 import time
-
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
 
@@ -29,24 +28,33 @@ class TSPR():
 		self.tour_cost = 0
 		self.starting_node = np.random.choice(self.cities)
 
-
-	'''
-		Private Methods
-	'''
-
 	def create_model(self, path):
 		"""
 			Create Graph representation and MDP variables with number of states designated
+			path: path of instance
 		"""
 		data_format = path.split('/')[1]
 		
 		if data_format == 'edge_explicity':
-			return self.read_edge_explicity_data(path)
+			return self.read_edge_explicity_instances(path)
 
 		elif data_format == 'node_coordinate':
-			return self.read_node_coordinate_data(path)
+			return self.read_node_coordinate_instances(path)
 
-	def read_edge_explicity_data(self, path):
+	def read_edge_explicity_instances(self, path):
+		'''
+			Read explicity edge instance
+			path: path of instance
+			ex:
+
+			5   			 <- number of nodes
+			1231 232 12 122  <- edges (0,1)(0,2)(0,3)(0,4)
+			336 1161 717 	 <- edges (1,2)(1,3)(1,4)
+			848 323			 <- edges (2,3)(2,4)
+			212				 <- edges (3,4)
+
+			return: Graph with weights
+		'''
 		with open(path) as reader:
 			# # Complete graph
 			G = nx.complete_graph(int(reader.readline()))
@@ -58,15 +66,29 @@ class TSPR():
 					G.edges[j, i]['w'] = w
 		return G
 
-	def read_node_coordinate_data(self, path):
+	def read_node_coordinate_instances(self, path):
+		'''
+			Read node coordinate instances
+			path: path of instance
+			ex:
+			
+			5     <- number of nodes
+			1 1   <- node 0, coordinate (x, y) = (1, 1) 
+			2 3   <- node 0, coordinate (x, y) = (2, 3) 	
+			4 5   <- node 0, coordinate (x, y) = (4, 5) 
+			3 3   <- node 0, coordinate (x, y) = (3, 3) 
+			7 5   <- node 0, coordinate (x, y) = (7, 5) 
+
+			return: Graph with weights
+		'''
 		with open(path) as reader:
 			G = nx.complete_graph(int(reader.readline()))
 			X = []
 			Y = []
 			for _ in range(G.number_of_nodes()):
 				x, y = reader.readline().split()
-				X.append(int(x))
-				Y.append(int(y))
+				X.append(float(x))
+				Y.append(float(y))
 			coordinates = np.array(list(zip(X, Y)))
 			dist_matrix = squareform(pdist(coordinates))
 			for i in range(G.number_of_nodes()):
@@ -77,6 +99,14 @@ class TSPR():
 		return G
 
 	def create_adj_mat(self, tour):
+		'''
+			Adjacency matrix to nn algorithm purposes
+			tour: partial ou complete tour to create adjacency matrix, ex:
+			[0, 2, 4]  < for 5 cities [0, 1, 2, 3, 4, 5] and starting city is 0 
+			neighbor with travel cost equal inf is never a good option, 
+			so cities in tour recieve this travel cost.
+			return: adjacency matrix
+		'''
 		# Adjacency matrix
 		adj_mat = np.array(nx.adjacency_matrix(self.Graph, weight='w').todense(), dtype=float)
 		# set main diagonal to inf
@@ -85,10 +115,15 @@ class TSPR():
 		for i in range(self.num_cities):
 			for t in tour[:-1]:
 				adj_mat[i][t] = np.inf
-
 		return adj_mat
 
 	def calc_cost(self, tour):
+		'''
+			Calc cost of tour.
+			tour: tour of traveler, ex:
+			[0, 2, 4, 1, 3, 0]  <- for 5 cities and starting city is 0 
+			return: cost of tour
+		'''
 		cost = 0
 		for i in range(self.num_cities):
 			cost += self.Graph[tour[i]][tour[i+1]]['w']
@@ -96,9 +131,11 @@ class TSPR():
 
 	def nearest_neighbor(self, tour):
 		'''
-			tour: is partial tour of solution, 
-			example, 5 cities, tour is list of nodes [0, 1, 2], index 0 is starting city,
+			Base heuristic to rollout algorithm, the main difference of traditional 
+			nearest neighbor algorithm is the beginning  with a partial tour (solution), ex:
+			tour is list of nodes [0, 1, 2], for 5 cities and starting city is 0,
 			the return is a complete tour, in the form [0, 1, 2, 4, 3, 0] and cost of tour 
+			
 		'''
 		# Create adjacency matrix
 		adj_mat = self.create_adj_mat(tour)
@@ -113,10 +150,6 @@ class TSPR():
 		# Calculate cost
 		cost = self.calc_cost(tour)
 		return tour, cost
-
-	'''
-		Public Methods
-	'''
 
 	def run(self):
 		self.tour = [self.starting_node]
@@ -152,46 +185,45 @@ class TSPR():
 		edge_labels = nx.get_edge_attributes(self.Graph, 'w')
 		nx.draw_networkx_edge_labels(self.Graph, pos, edge_labels)
 
-
 #%%
+def experiments_with(file_path):
+	# Instanciate TSPR
+	exp = TSPR(file_path)
+	# Experiment with tsp rollout algorithm
+	init_time = time.time()
+	_, rol_cost = exp.run()
+	rol_time = time.time() - init_time
+
+	# Experiment with tsp nearest neighbor algorithm
+	init_time = time.time()
+	_, nn_cost = exp.nearest_neighbor([exp.starting_node])
+	nn_time = time.time() - init_time
+
+	return rol_cost, rol_time, nn_cost, nn_time
+
 if __name__ == "__main__":
-	edge_explicity_data = os.listdir('data/edge_explicity')
-	node_coordinate_date = os.listdir('data/node_coordinate')
+	# Read Instances
+	folders = os.listdir('instances')
+	instances = dict()
+	instances['edge_explicity'] = os.listdir('instances/edge_explicity')
+	instances['node_coordinate'] = os.listdir('instances/node_coordinate')
+	
+	results = open(f'experiments/results{time.time_ns()}.txt', 'w')
+	results.write('instance_name,rol_cost,rol_time,nn_cost,nn_time\n')
+	n_episodes = 3
+	# results = []
+	for folder in folders:
+		for instance in instances[folder]:
+			file_path = f'instances/{folder}/{instance}'
+			# result = [instance]
+			results.write(instance)
+			mean_result = np.zeros(4)
+			for e in range(n_episodes):
+				mean_result += np.array(experiments_with(file_path))
+			mean_result /= n_episodes
+			# results.append(result + list(mean_result))
+			results.write(f',{mean_result[0]},{mean_result[1]},{mean_result[2]},{mean_result[3]}\n')
+	results.close()
+			
 
-	# Experiments
-	path = 'data/edge_explicity/'
-	results = dict()
-	n_episode = 5
-	for sample in edge_explicity_data:
-		tsp_cost_mean = 0
-		tsp_time_mean = 0
-		nn_cost_mean = 0
-		nn_time_mean = 0
-		for episode in range(n_episode):
-			exp = TSPR(path + sample)
-
-			init_time = time.time()
-			_, cost = exp.run()
-			tsp_time_mean += time.time() - init_time
-			tsp_cost_mean += cost
-
-			init_time = time.time()
-			_, cost = exp.nearest_neighbor([exp.starting_node])
-			nn_time_mean += time.time() - init_time
-			nn_cost_mean += cost
-
-		print(f'\nTSP sample: {sample}, in {n_episode} episodes')
-		print('\nRollout Algorithm')
-		print(f'Time mean: {tsp_time_mean / n_episode}')
-		print(f'Cost mean: {tsp_cost_mean / n_episode}')
-		print('\nNearest Neighbor Algorithm')
-		print(f'Time mean: {nn_time_mean / n_episode}')
-		print(f'Cost mean: {nn_cost_mean / n_episode}')
-
-
-
-# %%
-tour, cost = t.nearest_neighbor([t.starting_node])
-print('Tour NN sol:', tour)
-print('Tour NN cost:', cost)
 # %%
