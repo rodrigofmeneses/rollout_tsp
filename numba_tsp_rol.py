91ec45443bcb53e0737fb450408517098c52383c
Rodrigo Meneses <rodrigo.menesesufc@gmail.com>
2021-01-18 18:32:35 -0300

    add files and modify read coordinate function

--- numba_tsp_rol.py
+++ numba_tsp_rol.py
@@ -50,17 +50,22 @@ def read_node_coordinate_instances(path):
         return: Graph with weights
     '''
     with open(path) as reader:
-        G = nx.complete_graph(int(reader.readline()))
+        dimension = ' '
+        while dimension[0] != 'DIMENSION': dimension = reader.readline().split()
+        while reader.readline().split()[0] != 'NODE_COORD_SECTION': pass
+        dimension = int(dimension[-1])
+
+        G = nx.complete_graph(dimension)
         X = []
         Y = []
-        for _ in range(G.number_of_nodes()):
-            x, y = reader.readline().split()
+        for _ in range(dimension):
+            _ , x, y = reader.readline().split()
             X.append(float(x))
             Y.append(float(y))
         coordinates = np.array(list(zip(X, Y)))
         dist_matrix = squareform(pdist(coordinates))
-        for i in range(G.number_of_nodes()):
-            for j in range(G.number_of_nodes()):
+        for i in range(dimension):
+            for j in range(dimension):
                 if i != j:
                     G.edges[i, j]['w'] = dist_matrix[i, j]
                     G.edges[j, i]['w'] = dist_matrix[j, i]
@@ -81,56 +86,36 @@ def create_model(path):
     else:
         raise Exception
 
-@jit(nopython=True)
-def update_adj_mat(tour, num_cities, adj_mat):
+def nearest_neighbor(tour, adj_mat):
     '''
-        Adjacency matrix to nn algorithm purposes
-        tour: partial ou complete tour to create adjacency matrix, ex:
+        Base heuristic to rollout algorithm, the main difference of traditional.
+        nearest neighbor algorithm is the beginning  with a partial tour (solution), ex:
+        tour is list of nodes [0, 1, 2], for 5 cities and starting city is 0,
+        the return is a complete tour, in the form [0, 1, 2, 4, 3, 0] and cost of tour.
+    '''
+    nn_tour = nn_core(tour, adj_mat.copy())
+    nn_cost = calculate_tour_cost(tour, adj_mat.copy())
+
+    return nn_tour, nn_cost
+
+@jit(nopython=True)
+def nn_core(tour, adj_mat):
+    '''
+        Core of nearest neighbor, numba require only loops and numpy operations.
+        Update adjacency matrix to nn algorithm purposes, ex:
         [0, 2, 4]  < for 5 cities [0, 1, 2, 3, 4, 5] and starting city is 0 
         neighbor with travel cost equal inf is never a good option, 
         so cities in tour recieve this travel cost.
-        return: adjacency matrix
+        
     '''
-    # Adjacency matrix
-    #adj_mat = np.array(nx.adjacency_matrix(G, weight='w').todense(), dtype=float)
-    #adj_mat = ADJ_MAT.copy()
+    num_cities = adj_mat.shape[0]
+    # Update adjacency matrix
     # set main diagonal to inf
     for i in range(num_cities): adj_mat[i][i] = np.inf
     # set elements of current tour to inf
     for i in range(num_cities):
-        for t in tour[:-1]:
-            adj_mat[i][t] = np.inf
-    return adj_mat
+        for t in tour[:-1]: adj_mat[i][t] = np.inf
 
-
-@jit(nopython=True)
-def calculate_cost(tour, adj_mat):
-    '''
-        Calc cost of tour.
-        tour: tour of traveler, ex:
-        [0, 2, 4, 1, 3, 0]  <- for 5 cities and starting city is 0 
-        return: cost of tour
-    '''
-    cost = 0.
-    for i in range(len(tour) - 1):
-        cost += adj_mat[tour[i]][tour[i+1]]
-    return cost
-
-# def nn_core(tour):
-
-#     nn_tour = nearest_neighbor()
-#     nn_cost = 
-#     return nn_tour, nn_cost
-
-@jit(nopython=True)
-def nearest_neighbor(tour, num_cities, adj_mat):
-    '''
-        Base heuristic to rollout algorithm, the main difference of traditional 
-        nearest neighbor algorithm is the beginning  with a partial tour (solution), ex:
-        tour is list of nodes [0, 1, 2], for 5 cities and starting city is 0,
-        the return is a complete tour, in the form [0, 1, 2, 4, 3, 0] and cost of tour 
-        
-    '''
     # Complete tour 
     for _ in range(num_cities - len(tour)):
         min_index = np.argmin(adj_mat[tour[-1]])
@@ -142,11 +127,24 @@ def nearest_neighbor(tour, num_cities, adj_mat):
     # Return complete tour
     return tour
 
-def rollout_algorithm(G, adj_mat, num_cities, starting_node):
+@jit(nopython=True)
+def calculate_tour_cost(tour, adj_mat):
+    '''
+        Calc cost of tour.
+        tour: tour of traveler, ex:
+        [0, 2, 4, 1, 3, 0]  <- for 5 cities and starting city is 0 
+        return: cost of tour
+    '''
+    cost = 0.
+    for i in range(len(tour) - 1):
+        cost += adj_mat[tour[i]][tour[i+1]]
+    return cost
 
-    
+def rollout_algorithm(G, adj_mat, starting_node):
     # Starting tour
     tour = [starting_node]
+    
+    num_cities = adj_mat.shape[0]
 
     for _ in range(num_cities - 1):
         current_tour = tour.copy()
@@ -156,59 +154,53 @@ def rollout_algorithm(G, adj_mat, num_cities, starting_node):
         for j in set(G[tour[-1]]) - set(tour):
             current_tour.append(j)
 
-            current_adj_mat = update_adj_mat(current_tour.copy(), num_cities, adj_mat.copy())
-            nn_path = nearest_neighbor(current_tour.copy(), num_cities, current_adj_mat.copy())
+            _, rollout_cost = nearest_neighbor(current_tour.copy(), adj_mat.copy())
             
-            rollout_cost = calculate_cost(nn_path, adj_mat.copy())
             if rollout_cost < best_rollout_cost:
                 best_rollout_cost = rollout_cost
                 best_next_city = j
-            
+
             current_tour.pop()
         tour.append(best_next_city)
     tour.append(starting_node)
 
-    return tour, calculate_cost(tour, adj_mat.copy())
+    return tour, calculate_tour_cost(tour, adj_mat.copy())
 
 def experiments_with(file_path):
     # Create Model (Graph)
     G = create_model(file_path)
-
     # Create Adjacency Matrix
     adj_mat = np.array(nx.adjacency_matrix(G, weight='w').todense(), dtype=float)
     # number of cities
-    num_cities = G.number_of_nodes()
+    num_cities = adj_mat.shape[0]
     # random starting node
     starting_node = int(np.random.choice(range(num_cities)))
 
     # execute rollout algorithm and calculate time
     start = time.time()
-    rollout_tour, rollout_cost = rollout_algorithm(G, adj_mat, num_cities, starting_node)
+    rollout_tour, rollout_cost = rollout_algorithm(G, adj_mat, starting_node)
     rollout_time = time.time() - start
     
     # execute nearest neighbor algorithm and calculate time
     start = time.time()
-    current_adj_mat = update_adj_mat([starting_node], num_cities, adj_mat.copy())
-    nn_tour = nearest_neighbor([starting_node], num_cities, current_adj_mat.copy())
-    nn_cost = calculate_cost(nn_tour, adj_mat.copy())
+    nn_tour, nn_cost = nearest_neighbor([starting_node], adj_mat.copy())
     nn_time = time.time() - start
 
     return rollout_cost, rollout_time, nn_cost, nn_time
 
 #%%
 # Read Instances
-folders = os.listdir('instances')[:1]
+folders = os.listdir('instances')[1:2]
 # Create Results file
 results = open(f'experiments/results{time.strftime("%d%b%Y_%H_%M_%S", time.gmtime())}.txt', 'w')
 # Write header
 results.write('instance_name,rol_cost,rol_time,nn_cost,nn_time\n')
 
-
 # Start tests
-n_episodes = 10
+n_episodes = 1
 
 for folder in folders:
-    for instance in os.listdir(f'instances/{folder}'):
+    for instance in os.listdir(f'instances/{folder}')[:3]:
         file_path = f'instances/{folder}/{instance}'
         # Write instance name
         results.write(instance)
